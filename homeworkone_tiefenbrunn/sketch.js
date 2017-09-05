c5f14c8fa1e1e16cdbee1ec625490795d93efb64
swiftasadoe <31459536+swiftasadoe@users.noreply.github.com>
2017-09-05 19:23:24 -0400

    Initial Upload Week 1 Homework

---
+++ homeworkone_tiefenbrunn/sketch.js
@@ -0,0 +1,47 @@
+function setup() { 
+    createCanvas (1024,768);
+}
+
+function draw() {
+    background (20,200,215);
+    fill(255,227,26);
+    noStroke();
+    ellipse(553,300,150,160);
+    fill(0);
+    ellipse(575,275,20,20);
+    fill(255,227,26)
+    ellipse(525,445,300,200);
+    fill(255,163,29);
+    beginShape();
+        curveVertex(637,260);
+        curveVertex(603.17,291.21);
+        curveVertex(606.41,318.32);
+        curveVertex(616,331);
+        curveVertex(657.32,337.61);
+        curveVertex(679,328);
+        curveVertex(679.79,328.3);
+        curveVertex(661,301);
+        curveVertex(665.27,299.4);
+        curveVertex(694,276);
+        curveVertex(658,268);
+    endShape();
+    fill(117,225,229,100)
+    ellipse(40,200,200,200);
+    fill(117,225,229,100)
+    ellipse(240,400,20,20);
+    fill(117,225,229,100)
+    ellipse(340,60,180,180);
+    fill(117,225,229,100)
+    ellipse(40,200,200,200);
+    fill(117,225,229,100)
+    ellipse(600,600,100,100);
+    fill(117,225,229,100)
+    ellipse(725,550,70,70);
+    
+    
+    
+   
+    
+   
+//    (637,260,603,291,606,318,616,331,657,337,679,329,680,328,661,301,665,299,694,276,658,268);
+}
