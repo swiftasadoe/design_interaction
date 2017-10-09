1cd76c77f25692fd5af249301d82893511bb522d
swiftasadoe <31459536+swiftasadoe@users.noreply.github.com>
2017-10-08 21:39:09 -0400

    Week 5 Homework

--- sketch.js
+++ sketch.js
@@ -1,52 +1,69 @@
-function setup(){  
-createCanvas(1000,400);  
-background(114,87,38);
+var rectX = 200;
+var rectY = 200;
+var rectSize = 500;
+var rectTop = rectY;
+var rectBottom = rectY + rectSize;
+var rectLeft = rectX;
+var rectRight = rectX + rectSize;
+var colR;
+var getFunky = false; 
+//var quicksand;
+//
+//function preload () {
+//    quicksand = loadFont("Quicksand.ttf");
+//    
+//}
 
-    //glasses
-    noFill();
-    strokeWeight(8);
-    rect(66,99,243,214);
-    rect(600,99,243,214);
-    line(310,214,600,214);
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+//    textFont(quicksand);
 }
 
+function draw() {
+    noStroke();
+    fill(colR,100,20);
+    fill(150,75,240);
+    rect(190,120,520,80);
+    rect(430,200,20,rectSize);
+    noStroke();
+    fill(colR, 100, 60);
+    rect(rectX,rectY,rectSize,rectSize);
 
-function draw(){
+     if(mouseX > rectLeft && mouseX < rectRight && mouseY > rectTop && mouseY < rectBottom){
+         fill(mouseX,mouseY,30);
+         textSize(50);
+         text("Wanna see what's inside?", mouseX,mouseY, 200,400);    
+       }else{
+           fill(colR,mouseX,mouseY);
+            textSize(50);
+           text("surprise!", mouseX, mouseY,100,100);
+       } 
+
     
-
-
- if(state == 1){  
-//eyes
-    fill(2);
-    ellipse(190,218,120,120);
-    ellipse(720,218,120,120);
-    fill(250);
-    ellipse(190,230,80,80);
-    ellipse(720,230,80,80);  
- }else if(state == 2){
-    fill(84,66,211);
-    ellipse(190,218,120,120);
-    ellipse(720,218,120,120);
-}else if (state == 3){ 
-    fill(11,26,112);
-    ellipse(190,218,120,120);
-    ellipse(720,218,120,120);
-}else if (state == 4){
-    fill(11,112,79);
-    ellipse(190,218,120,120);
-    ellipse(720,218,120,120);
-}
-else if (state == 5){
-    fill(11,112,23);
-    ellipse(190,218,120,120);
-    ellipse(720,218,120,120);
-}
     
-    function mousePressed(){
-    if(state < 5){
-    state = state +1;
-    }else{
-    state =1;            
-    }
 }
+
+function mousePressed(){
+        if(mouseX > rectLeft && mouseX < rectRight && mouseY > rectTop && mouseY < rectBottom && getFunky==false) 
+//            IF all these things happen THEN
+            {
+           colR = 50; 
+//            this is the value that changes 
+         getFunky = !getFunky;     
+//            switches values so you can go back and forth, not just one click  
+         background(230,100,200);  
+
+         fill(0);
+         textSize(120);
+         text("PARTY WHOO",700,300, 500, 500);
+       }else if(mouseX > rectLeft && mouseX < rectRight && mouseY > rectTop && mouseY < rectBottom && getFunky==true){
+           colR = 220;
+           getFunky = !getFunky;
+           background(60,170,200); 
+           fill(0);
+         textSize(120);
+           text("PARTY YEAH",700,300,500,500);
+           
+       } 
+//THIS WILL ONLY WORK WITH TWO BECAUSE WE USE TRUE/FALSE/BOOLEAN
 }
